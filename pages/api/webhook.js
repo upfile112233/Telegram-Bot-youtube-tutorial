bdaf90a35705774fcb9b0ff7ea57a532fbf142b5
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-11-13 13:50:36 +0530

    commit

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { cricketCommand } from "@/utils/commands/cricket";
+import { helpCommand } from "@/utils/commands/help";
+import { pingCommand } from "@/utils/commands/ping";
 import { sendMessage } from "@/utils/telegram";
-
-// 1. /start, /help, 
 
 export default async function handler(req, res) {
   if (req.method=="POST") {
@@ -10,9 +11,16 @@ export default async function handler(req, res) {
     const text = req.body.message.text;
     console.log("ChatID", chatId);
     console.log("text", text);
-    if (text=="ping") {
-      await sendMessage(chatId,"pong");
-    } else {
+    if (text.startsWith("/start") || text.startsWith("/help") ) {
+      await helpCommand(chatId)
+    }
+    else if (text.startsWith("/ping")){
+      await pingCommand(chatId);
+    }
+    else if (text.startsWith("/cricket")) {
+      await cricketCommand(chatId);
+    } 
+    else {
       await sendMessage(chatId,text);
     }
     res.status(200).send("OK")
